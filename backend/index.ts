3725bacf81190fa73cb272c515d1c6fcf516f00a
Lukasz-Lipinski <l.lipinski1993@gmail.com>
2023-03-30 10:52:56 +0200

    structure is done

---
+++ backend/index.ts
@@ -0,0 +1,45 @@
+import express, {
+  Request,
+  Response,
+} from 'express';
+import path from 'path';
+import bodyparser from 'body-parser';
+import cors from 'cors';
+import dotenv from 'dotenv';
+
+const app = express();
+const port = process.env.PORT || '5000';
+
+dotenv.config();
+
+app.use(
+  cors(),
+  express.urlencoded({ extended: true }),
+  express.static(
+    path.join(
+      process.cwd(),
+      '/frontend/dist/color-carts'
+    )
+  ),
+  bodyparser.urlencoded({ extended: true }),
+  bodyparser.json()
+);
+
+app.get('/', (req: Request, res: Response) => {
+  res.json({
+    msg: 'hello world!',
+  });
+});
+
+app.use('/', (req: Request, res: Response) => {
+  res.sendFile(
+    path.join(
+      process.cwd(),
+      '/frontend/dist/color-carts/index.html'
+    )
+  );
+});
+
+app.listen(port, () => {
+  console.log('This app is running on' + port);
+});
